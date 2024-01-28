f7dde035fd7ad7b6ad677a723ebb347a6b3a0d8f
lewis-andy <lewisandanje3@gmail.com>
2024-01-28 22:07:00 +0300

    text

---
+++ damn.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class damn {
+    public static void main(String[]args){
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("Enter your age in number:");
+        int age = Scanner.nextShort();
+        System.out.println(" Your age is " + age);
+    }
+
+}
