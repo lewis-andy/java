befc1dd9ab564036fde78a2e7a3655a583cfd964
lewis-andy <lewisandanje3@gmail.com>
2024-03-05 00:04:54 +0300

    Phace of java

---
+++ lesson/src/shape.java
@@ -0,0 +1,12 @@
+public class shape {
+    public static void main(String[]args){
+        for (int i =>10; i=1;i++){
+            for (int j =1;j<=10;j++){
+                System.out.print("z" +"   ");
+            }//End of innnerloop
+            System.out.println();
+        }//End of outer loop
+    }//end class
+}// end main
+
+
